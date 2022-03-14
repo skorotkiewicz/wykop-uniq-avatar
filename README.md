b0ea3ee803ca15764396d07b9069c5255ab34d2a
Sebastian Korotkiewicz <skorotkiewicz@gmail.com>
2022-03-14 11:44:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Wykop Uniq Users Avatars
+
+## How to install
+
+Download [Tampermonkey](https://www.tampermonkey.net)
+
+- [Firefox](https://addons.mozilla.org/en-US/firefox/addon/tampermonkey/)
+- [Chrome](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo)
+- [Opera](https://addons.opera.com/en/extensions/details/tampermonkey-beta/)
+- [Safari](https://apps.apple.com/us/app/tampermonkey/id1482490089?mt=12)
+- [Microsoft Edge](https://microsoftedge.microsoft.com/addons/detail/tampermonkey/iikmkjmpaadaobahmlepeloendndfphd)
+
+After installing the addon, click on the Tampermonkey icon, click on "Add new script" a window will open in which you have to paste the contents of the index.js file.
+
+After pasting, do not forget to save the file, you can do this using the shortcut Ctrl+S or File->Save
+
+## How it looks
+
+![screenshot](screenshot.png)
---
+++ index.js
@@ -0,0 +1,66 @@
+// ==UserScript==
+// @name         Wykop Uniq Avatar
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Avatars for the Wykop.pl generated from user nickname
+// @author       skorotkiewicz
+// @match        https://www.wykop.pl/wpis/*
+// @match        https://www.wykop.pl/mikroblog/*
+
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=wykop.pl
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  let observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry, i) => {
+        if (entry.isIntersecting) {
+          const p = 2;
+          const c = document.createElement("canvas");
+          const x = c.getContext("2d");
+          c.width = 18;
+          c.height = 14;
+          const s = entry.target.innerText;
+          const r = 1;
+
+          if (s) {
+            for (
+              let s = entry.target.innerText, r = 1, i = 28 + s.length;
+              i--;
+
+            ) {
+              // xorshift32
+              (r ^= r << 13), (r ^= r >>> 17), (r ^= r << 5);
+              const X = i & 3,
+                Y = i >> 2;
+              if (i >= 28) {
+                // seed state
+                r += s.charCodeAt(i - 28);
+                x.fillStyle =
+                  "#" + ((r >> 8) & 0xffffff).toString(16).padStart(0, 6);
+              } else {
+                // draw pixel
+                if (r >>> 29 > (X * X) / 3 + Y / 2)
+                  x.fillRect(p * 3 + p * X, p * Y, p, p),
+                    x.fillRect(p * 3 - p * X, p * Y, p, p);
+              }
+            }
+          }
+
+          entry.target.prepend(c);
+        } else {
+          if (entry.target.firstChild.tagName === "CANVAS")
+            entry.target.firstChild.remove();
+        }
+      });
+    },
+    { rootMargin: "0px 0px 0px 0px" }
+  );
+
+  document.querySelectorAll(".author a>b").forEach((user) => {
+    observer.observe(user);
+  });
+})();
